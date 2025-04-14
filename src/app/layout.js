2839f92a5029a5030956f2e7e3ccb057d76336d5
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-04-15 04:19:14 +0530

    Adding Auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignInButton, SignUpButton, SignedOut, UserButton, SignedIn } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +19,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn> 
+        </header>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/app/survey/page.jsx
+++ src/app/survey/page.jsx
@@ -1,12 +1,18 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Header from "../components/organisms/header";
-import Questionnarie from "../components/templates/GRIquestionnarie";
 import Topic from "../components/templates/topics";
 
 export default function Question() {
     return (
         <>
+        <SignedIn>
+        <UserButton />
         <Header/>
         <Topic/>
+        </SignedIn>
+        <SignedOut>
+        <h1>Please sign in to continue</h1>
+        </SignedOut>
         </>
     )
 }
